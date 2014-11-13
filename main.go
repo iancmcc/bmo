19bd856d9937026f88d72700935fd32efb73c855
Ian McCracken <ian.mccracken@gmail.com>
2014-11-12 19:31:00 -0600

    Hooray, can print state

--- main.go
+++ main.go
@@ -20,14 +20,14 @@ const (
 )
 
 func discover(target string) <-chan goupnp.MaybeRootDevice {
-	if devices, err := goupnp.DiscoverDevices(target); err != nil {
+	devices, err := goupnp.DiscoverDevices(target)
+	if err != nil {
 		log.Printf("Unable to discover %s devices: %v\n", target, err)
 		out := make(chan goupnp.MaybeRootDevice)
 		close(out)
 		return out
-	} else {
-		return devices
 	}
+	return devices
 }
 
 type WeMoDevice struct {
@@ -52,11 +52,16 @@ func (client *BasicEventClient) GetBinaryState() string {
 	response := &struct {
 		BinaryState string
 	}{}
-	err := client.SOAPClient.PerformAction(URN_BasicEvent_1, "GetBinaryState", request, response)
-	fmt.Printf("THEERR: %+v\n", err)
+	if err := client.SOAPClient.PerformAction(URN_BasicEvent_1, "GetBinaryState", request, response); err != nil {
+		fmt.Printf("THEERR: %+v\n", err)
+		return ""
+	}
 	binaryState, err := soap.UnmarshalString(response.BinaryState)
+	if err != nil {
+		fmt.Printf("Err: %+v\n", err)
+		return ""
+	}
 	fmt.Printf("BS: %+v\n", response.BinaryState)
-	fmt.Printf("Err: %+v\n", err)
 	return binaryState
 }
 
@@ -67,27 +72,9 @@ func main() {
 	insights := discover(URN_Insight)
 
 	for device := range mergeDevices(switches, motions, lightswitches, insights) {
+		fmt.Println(device)
 		sw := &WeMoDevice{device.Root}
+		fmt.Println(sw.Root.Device.FriendlyName)
 		fmt.Println(sw.GetBasicEventClient().GetBinaryState())
-		//dev := device.Root.Device
-		//svc := dev.FindService(URN_BasicEvent_1)
-		//fmt.Printf("%+v", svc)
-		//client := goupnp.ServiceClient{
-		//	SOAPClient: svc.NewSOAPClient(),
-		//	RootDevice: device.Root,
-		//	Service:    svc,
-		//}
-		//scpd, _ := svc.RequestSCDP()
-		//for _, action := range scpd.Actions {
-		//	fmt.Printf(" * %s\n", action.Name)
-		//	for _, arg := range action.Arguments {
-		//		var varDesc string
-		//		if stateVar := scpd.GetStateVariable(arg.RelatedStateVariable); stateVar != nil {
-		//			varDesc = fmt.Sprintf(" (%s)", stateVar.DataType.Name)
-		//		}
-		//		fmt.Printf("    * [%s] %s%s\n", arg.Direction, arg.Name, varDesc)
-		//	}
-		//}
 	}
-	log.Println("Done with a run")
 }
